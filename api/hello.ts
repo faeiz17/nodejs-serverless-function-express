a06a7bbb37b4e1934816482bdd108cddf654fcbf
Muhammad faeiz furqan <114372023+faeiz17@users.noreply.github.com>
2024-03-17 20:14:20 +0500

    Update hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -1,15 +1,8 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
+import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-const handler = (req: VercelRequest, res: VercelResponse) => {
-  const { name = 'World' } = req.query;
-
-  const responseObject = {
-    pictureurl: "https://configurator.porsche.com/model-start/pictures/718/extcam03.webp",
-    picturename: "718"
-  };
-
-  return res.json(responseObject);
-};
-
-export default handler;
-
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const { name = 'World' } = req.query
+  return res.json({
+    message: `Hello ${name}!`,
+  })
+}
